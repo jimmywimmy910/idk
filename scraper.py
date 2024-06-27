a2c52de1a261465fac394829136b2202d62a63f3
jimmywimmy910 <157238884+jimmywimmy910@users.noreply.github.com>
2024-06-27 02:48:12 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,17 +2,6 @@ import discord
 import requests
 from discord.ext import tasks
 
-# Replace with your actual values
-TOKEN = 'MTA5NTcwOTA2MzQ2NTE1MjU5Mw.Gk62Nc.xnkmp6cPFRjHRIsnGOIOmP31IrwPl80nIw7Q5U'
-CHANNEL_ID = 1252764057652428861  # Replace with your channel ID
-NEOCITIES_API_KEY = '9b7a1c8b2adcfc713062f2143d671da0'
-
-client = discord.Client(intents=discord.Intents.default())
-
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}')
-    fetch_links.start()
 
 @tasks.loop(minutes=10)
 async def fetch_links():
